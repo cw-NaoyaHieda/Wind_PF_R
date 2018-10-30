e9d479c17ace277de3246d875e30d6d9c529659f
naoya12185 <hero12185@gmail.com>
2018-10-30 18:27:16 +0900

    add not theta

--- Estep_not_theta.R
+++ Estep_not_theta.R
@@ -34,24 +34,19 @@ y = sample$y;
 v = sample$v;
 if(state == 1){
   alp = sample$alp;
-  rho1 = sample$r;
+  rho_ans = sample$r;
 }
 
 
 
 
 par1 = c(phi1, gam, mu_g, mu_f, rho_f, V, mu_rho, sig_rho);
-  
+
 #　並列化フィルタリング
-hosts <- rep('localhost',6)
-scl <- makeCluster(hosts)
-clusterExport(scl,c('r_conditional_WJ_mean','d_conditional_WJ','psswrappedcauchy','dsswrpcauchy','circular.mean'))
-X <- particlefilter_theta_estimate(par1, y, v, 100)
-stopCluster(scl)
+X <- particlefilter(par1, y, v, 100)
 
 
 pfOut1 <- X$pfOut1
-pfOut2 <- X$pfOut2
 rho1 <- X$rho1
 wt <- X$wt
 
@@ -59,26 +54,7 @@ wt <- X$wt
 smwt<-particlesmoother(phi, pfOut1, wt)
 
 
-# 色々Plot thetaから
-# thetaを計算するために、各時点でのcosとsinからcircularmean
-pfOut2_sin <- rowSums(wt * sin(pfOut2[-1,]))
-pfOut2_cos <- rowSums(wt *cos( pfOut2[-1,]))
-pfOut2_tri <- data.frame(pfOut2_sin, pfOut2_cos)
-mean_theta <- apply(pfOut2_tri, 1,function(x) circular.mean(x[1], x[2]))
-
-pfOut2_sm_sin <- rowSums(sin(smwt * pfOut2[-1,]))
-pfOut2_sm_cos <- rowSums(cos(smwt * pfOut2[-1,]))
-pfOut2_sm_tri <- data.frame(pfOut2_sin, pfOut2_cos)
-sm_theta <- apply(pfOut2_tri, 1,function(x) circular.mean(x[1], x[2]))
-
-
-check_smoothing <- data.frame(dt=1:length(mean_theta),mean_theta,sm_theta) %>%
-  cbind(answer = y[-length(y)])
-
-p1 <- ggplot(check_smoothing %>% gather(id,theta,-dt), aes(x=dt,y=theta,color=id)) +
-  geom_line()
-print(p1)
-
+# 色々Plot thetaなし
 
 # v
 mean_v = rowSums(par1[2]*exp(pfOut1/2)*wt)
@@ -107,7 +83,7 @@ print(p3)
 mean_rho = rowSums(wt * rho1)
 sm_rho = rowSums(smwt * rho1)
 if(state == 1){
-  check_smoothing <- data.frame(dt=1:length(mean_rho),mean_rho,sm_rho,answer = rho1[-length(rho1)])
+  check_smoothing <- data.frame(dt=1:length(mean_rho),mean_rho,sm_rho,answer = rho_ans[-length(rho_ans)])
 }else{
   check_smoothing <- data.frame(dt=1:length(mean_rho),mean_rho,sm_rho) 
 }
@@ -118,8 +94,8 @@ p4 <- ggplot(check_smoothing %>% gather(id,rho,-dt), aes(x=dt,y=rho,color=id)) +
   geom_line()
 print(p4)
 
-print(grid.arrange(p1, p2, p3, p4,
-                   ncol = 2))
+print(grid.arrange(p2, p3, p4,
+                   ncol = 1))
 
 pw_weight <- pairwise_weight(phi1, pfOut1, wt, smwt)
 
@@ -127,6 +103,6 @@ pw_weight <- pairwise_weight(phi1, pfOut1, wt, smwt)
 
 result <- Q_calc_Estep(par1, pfOut1, rho1, pw_weight, smwt, y, v)
 
-result
+print(result)
 
 
